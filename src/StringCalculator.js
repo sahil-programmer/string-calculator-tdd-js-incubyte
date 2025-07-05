533aebdef98e9aab970e8816ca57afda8cd603fb
sahil-programmer <sahiltarofawala312@gmail.com>
2025-07-05 15:38:47 +0530

    green: pass test for custom delimiter input like //;\n1;2

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -1,8 +1,17 @@
 class StringCalculator {
   add(numbers) {
     if (typeof numbers !== "string" || !numbers.trim()) return 0;
+    let input = numbers;
 
-    return numbers
+    if (numbers.startsWith("//")) {
+      const delimiterEndIndex = numbers.indexOf("\n");
+      const delimiter = numbers.substring(2, delimiterEndIndex);
+      input = numbers
+        .substring(delimiterEndIndex + 1)
+        .split(delimiter)
+        .join(",");
+    }
+    return input
       .replace(/\n/g, ",")
       .split(",")
       .map((n) => Number(n.trim()))
