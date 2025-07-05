88e4f3bfcc964804908c1960c160b914c036deaa
sahil-programmer <sahiltarofawala312@gmail.com>
2025-07-05 15:18:59 +0530

    red: add failing test for newline delimiter support

--- __tests__/StringCalculator.test.js
+++ __tests__/StringCalculator.test.js
@@ -17,4 +17,8 @@ describe("StringCalculator", () => {
     const calculator = new StringCalculator();
     expect(calculator.add("1,2,3,4")).toBe(10);
   });
+  test("returns the sum when numbers are separated by newline and comma", () => {
+    const calculator = new StringCalculator();
+    expect(calculator.add("1\n2,3")).toBe(6);
+  });
 });
